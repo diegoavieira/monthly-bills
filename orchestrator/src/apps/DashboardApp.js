d864602fc645018c148c9b6d8a17473b216799d5
Diego Adilson Vieira <diegoavieira@gmail.com>
2021-09-11 21:42:23 -0300

    build: adjustments

--- orchestrator/src/apps/DashboardApp.js
+++ orchestrator/src/apps/DashboardApp.js
@@ -1,28 +1,41 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 
-import mount from 'dashboard/DashboardApp';
+const mountPromise = import('dashboard/DashboardApp');
 
 const DashboardApp = () => {
   const ref = useRef(null);
   const history = useHistory();
+  const [errorLoading, setErrorLoading] = useState('');
 
   useEffect(() => {
-    const { onParentNavigate } = mount(ref.current, {
-      initialPath: history.location.pathname,
-      onNavigate: ({ pathname: nextPathname }) => {
-        const { pathname } = history.location;
+    setErrorLoading('');
 
-        if (pathname !== nextPathname) {
-          history.push(nextPathname);
-        }
-      }
-    });
+    mountPromise
+      .then((res) => {
+        const mount = res.default;
 
-    history.listen(onParentNavigate);
+        const { onParentNavigate } = mount(ref.current, {
+          initialPath: history.location.pathname,
+          onNavigate: ({ pathname: nextPathname }) => {
+            const { pathname } = history.location;
+
+            if (pathname !== nextPathname) {
+              history.push(nextPathname);
+            }
+          }
+        });
+
+        history.listen(onParentNavigate);
+      })
+      .catch(() => {
+        setErrorLoading('Error Loading');
+      });
+
+    return () => {};
   }, []);
 
-  return <div ref={ref} />;
+  return <div ref={ref}>{errorLoading}</div>;
 };
 
 export default DashboardApp;
